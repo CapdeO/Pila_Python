b32248c6806285549329877d9ba199d289c9896a
CapdeO <103727616+CapdeO@users.noreply.github.com>
2022-06-12 08:16:21 -0300

    Añadir función globalizar

--- Pila.py
+++ Pila.py
@@ -6,63 +6,60 @@ cant_elem = 0
 maximo = 0
 p = []
 
-def llena ():
+def globalizar():
+  global cima
   global pila_llena
+  global pila_vacia
+  global cant_elem
+  global maximo
+  global p
+
+def llena ():
+  globalizar()
   if maximo == cima + 1:
     pila_llena = True
-    
   else:
     pila_llena = False
-    
 
 def vacia():
-  global pila_vacia
+  globalizar()
   if cima == -1:
     pila_vacia = True
-    
   else:
     pila_vacia = False
     
-
 def crear_pila():
-  global p
+  globalizar()
   p = []
-  global maximo
   maximo = int(input("ingrese un máximo: "))
-  global cima
   cima = -1
 
 def cantidad():
-  global cant_elem
+  globalizar()
   cant_elem = cima + 1
 
 def apilar():
+  globalizar()
   llena()
   if pila_llena == True:
     print("La pila está llena")
   else:
     ingreso = int(input("Ingrese número: "))
     p.append(ingreso)
-    global cima
     cima = cima + 1
     print("La pila es: ", p)
-    global cant_elem
     cant_elem = cant_elem + 1
     
 
 def desapilar():
+  globalizar()
   vacia()
-
-  
   if pila_vacia == True:
     print("Pila vacía")
   else:
-    global cima
     p.pop(cima)
     cima = cima - 1
-    global cant_elem
     cant_elem = cant_elem - 1
-
 
 print("¿Qué quieres hacer? \n1) Crear pila\n2) Apilar\n3) Desapilar\n4) Saber si está llena\n5) Saber si está vacía\n6) Saber la cantidad de elementos\n7) Salir")
 corte = int(input("Elija una opción:"))
